0c77a1d79f358aeef0ceb82a065a72799e3ff4fc
Louis-Enajedu <enajeduokeoghene@gmail.com>
2024-06-21 22:20:44 +0100

    Initial commit

--- notes/urls.py
+++ notes/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import note_list, note_detail, note_create, note_update, note_delete
+from . import views
 
 urlpatterns = [
-    path('', note_list, name='note_list'),
-    path('note/<int:pk>/', note_detail, name='note_detail'),
-    path('note/new/', note_create, name='note_create'),
-    path('note/<int:pk>/edit/', note_update, name='note_update'),
-    path('note/<int:pk>/delete/', note_delete, name='note_delete'),
+    path('', views.note_list, name='note_list'),
+    path('<int:pk>/', views.note_detail, name='note_detail'),
+    path('new/', views.note_create, name='note_create'),
+    path('<int:pk>/edit/', views.note_update, name='note_update'),
+    path('<int:pk>/delete/', views.note_delete, name='note_delete'),
 ]
